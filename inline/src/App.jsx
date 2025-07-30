503ff924c8ac0fb90628cd2fb3843ac5f815f241
Matthew Boyer <lyfsux@MacBookPro.lan>
2025-07-30 15:06:01 -0500

    Initial setup w/button

---
+++ inline/src/App.jsx
@@ -0,0 +1,17 @@
+//  How to style React Components with css
+// ---------------------------------------
+// 1. External
+// 2. Modules
+// 3. INLINE
+
+function App() {
+  
+
+  return (
+    <Button />
+      
+   
+  )
+}
+
+export default App
---
+++ inline/src/Button.jsx
@@ -0,0 +1,8 @@
+
+
+function Button(){
+    // Using a dynamic value for className
+        // Using modules allows each element to have a unique algorithm 
+    return(<button className={styles.button}>CLICK ME</button>);
+}
+ export default Button
